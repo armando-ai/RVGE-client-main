093229ef0a8f86e78de56e824b026192ad9a2b8d
armando-ai <aramirezcanas@student.neumont.edu>
2023-03-05 20:34:56 -0700

    some error on search

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
+  productionBrowserSourceMaps: true,
+  compress: false,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
