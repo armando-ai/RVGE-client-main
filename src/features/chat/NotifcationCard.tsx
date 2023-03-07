c1b206a361e51ac324e999ff4f996a0dad9c8730
armando-ai <aramirezcanas@student.neumont.edu>
2023-03-07 11:41:46 -0700

    ready

--- src/features/chat/NotifcationCard.tsx
+++ src/features/chat/NotifcationCard.tsx
@@ -33,9 +33,9 @@ const NotificationCard = (props: any) => {
                   router.push("/rtrades");
                 };
           }}
-          className={`${props.className} fixed top-5 z-[9999] h-[10%] w-[24%] flex-col content-start justify-start rounded-md bg-slate-400 p-[1%]`}
+          className={`${props.className} fixed top-5 z-[9999] h-[10%] w-[24%] cursor-pointer flex-col content-start justify-start overflow-hidden rounded-md bg-slate-400 p-[1%]`}
         >
-          <div className="flex-row flex w-full p-[1%]">
+          <div className="flex-row flex w-full overflow-hidden p-[1%]">
             <BellAlertIcon className="mr-auto h-7 w-7"></BellAlertIcon>
             <div className="ml-auto h-full w-[80%] flex-col">
               <h1 className="mr-auto">{props.notification.from}</h1>
