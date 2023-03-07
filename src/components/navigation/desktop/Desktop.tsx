a99e7cb5d4d1a85bbb3f6f0b3a782ff06dd701a2
armando-ai <aramirezcanas@student.neumont.edu>
2023-03-07 15:53:40 -0700

    tf

--- src/components/navigation/desktop/Desktop.tsx
+++ src/components/navigation/desktop/Desktop.tsx
@@ -54,32 +54,14 @@ pt-[4%]`}
         {links.map((link) => {
           return (
             <li key={link.name}>
-              {!link.name.includes("Reports") && (
-                <Link
-                  className="relative mb-[50%] flex flex-col items-center justify-center overflow-hidden text-center text-gray-500 duration-200 hover:text-black"
-                  href={link.href}
-                >
-                  {link.name.includes("Reports") ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="h-6 w-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                      />
-                    </svg>
-                  ) : (
-                    link.icon
-                  )}
-                  <span className="overflow-hidden">{link.name}</span>
-                </Link>
-              )}
+              <Link
+                className="relative mb-[50%] flex flex-col items-center justify-center overflow-hidden text-center text-gray-500 duration-200 hover:text-black"
+                href={link.href}
+              >
+                {link.icon}
+
+                <span className="overflow-hidden">{link.name}</span>
+              </Link>
             </li>
           );
         })}
