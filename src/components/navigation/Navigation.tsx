c1f1163be8849e124c2d75998791df1ea586a4b8
armando-ai <aramirezcanas@student.neumont.edu>
2023-03-07 18:43:13 -0700

    -1

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -68,7 +68,7 @@ export const Navigation = (props: any) => {
       )}
 
       {notifications.map((notification: { id: any }, index: number) => {
-        if (index === notifications.length) {
+        if (index === notifications.length - 1-1) {
           removeNotification(notifications.at(0));
         }
         return (
